545e2f0305ab67b8267f77ba92755cf1c997cddc
Alexandr <digital-mag@ya.ru>
2024-01-07 20:41:28 +0300

    manual attempts

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,24 @@
-import svelte from 'rollup-plugin-svelte';
+//@ts-check
+// import svelte from 'rollup-plugin-svelte';
+import svelte from 'rollup-plugin-svelte-hot'
+import css from 'rollup-plugin-css-only';
+import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess';
+
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
+
+
+// TODO try 
+/**
+ * - TheBrockEllis/rollup-plugin-svelte-hot
+ * - https://github.com/rixo/rollup-plugin-hot
+ * - svelte-hmr
+ */
+
 
 function serve() {
 	let server;
@@ -37,15 +51,65 @@ export default {
 	},
 	plugins: [
 		svelte({
+			preprocess: sveltePreprocess({ sourceMap: !production }),
+
+			hot: {
+				// Prevent preserving local component state
+				preserveState: false,
+
+				// If this string appears anywhere in your component's code, then local
+				// state won't be preserved, even when noPreserveState is false
+				noPreserveStateKey: '@!hmr',
+				preserveStateKey: 'hmr',
+
+				// Prevent doing a full reload on next HMR update after fatal error
+				noReload: false,
+
+				// Try to recover after runtime errors in component init
+				optimistic: false,
+
+				// --- Advanced ---
+
+				// By default, when the hot option is enabled, the `css` option of this
+				// plugin (same option as official plugin) will be changed to `false`,
+				// because extracting CSS doesn't work with HMR currently. You can use
+				// this option to disable this behaviour if it cause problems with your
+				// setup.
+				noDisableCss: true,
+
+				// When you change only the <style> part of a component, then only the
+				// CSS will be reinjected. Existing component instances won't be
+				// recreated. Set `false` to force recreation.
+				injectCss: true,
+
+				// Delay to mitigate FOUC (flash of unstyled content) when a component
+				// is updated, before the new version with the new CSS is loaded.
+				cssEjectDelay: 100,
+
+				// Prevent adding an HMR accept handler to components with
+				// accessors option to true, or to components with named exports
+				// (from <script context="module">). This have the effect of
+				// recreating the consumer of those components, instead of the
+				// component themselves, on HMR updates. This might be needed to
+				// reflect changes to accessors / named exports in the parents,
+				// depending on how you use them.
+				// acceptAccessors: false,
+				// acceptNamedExports: false,
+
+				// Set true to enable support for Nollup (note: when not specified, this
+				// is automatically detected based on the NOLLUP env variable)
+				// nollup: false,
+			},
 			// enable run-time checks when not in production
-			dev: !production,
+			// dev: !production,
+			
 			// we'll extract any component CSS out into
 			// a separate file - better for performance
-			css: css => {
-				css.write('bundle.css');
-			}
+			// css: css => {
+			// 	css.write('bundle.css');
+			// }
 		}),
-
+		css({ output: 'bundle.css' }),
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration -
@@ -59,7 +123,7 @@ export default {
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
-		!production && serve(),
+		// !production && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <style>
 	p {
-		color: purple;
+		color: gray;
 		font-family: 'Comic Sans MS', cursive;
 		font-size: 2em;
 	}
 </style>
 
 <p>!B8;87>20=>!</p>
-<input type="text" placeholder="1">
+<input type="text" placeholder="12">
